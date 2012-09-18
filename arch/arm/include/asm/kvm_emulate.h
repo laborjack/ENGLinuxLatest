e9b7c72f760ee8834e0a0a1272c104a0a2acda21
Marc Zyngier <marc.zyngier@arm.com>
2012-09-18 12:07:06 +0100

    ARM: KVM: abstract IL decoding away
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>

--- arch/arm/include/asm/kvm_emulate.h
+++ arch/arm/include/asm/kvm_emulate.h
@@ -135,4 +135,10 @@ static inline int kvm_vcpu_dabt_get_as(struct kvm_vcpu *vcpu)
 	}
 }
 
+/* This one is not specific to Data Abort */
+static inline bool kvm_vcpu_trap_il_is32bit(struct kvm_vcpu *vcpu)
+{
+	return !!((kvm_vcpu_get_hsr(vcpu) >> 25) & 1);
+}
+
 #endif /* __ARM_KVM_EMULATE_H__ */
